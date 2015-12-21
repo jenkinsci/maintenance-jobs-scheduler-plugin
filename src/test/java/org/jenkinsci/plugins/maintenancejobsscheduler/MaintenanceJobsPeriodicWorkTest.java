ebf36c98b094f69dbe65e546c7ee0627abaebb52
Victor Martinez <victor.martinez@king.com>
2015-12-21 16:59:45 +0100

    Removed unit test case since I didn't finish that logic

--- src/test/java/org/jenkinsci/plugins/maintenancejobsscheduler/MaintenanceJobsPeriodicWorkTest.java
+++ src/test/java/org/jenkinsci/plugins/maintenancejobsscheduler/MaintenanceJobsPeriodicWorkTest.java
@@ -21,36 +21,4 @@ public class MaintenanceJobsPeriodicWorkTest {
         periodicWork = new MaintenanceJobsPeriodicWork();
     }
 
-    /**
-     * Depends on test testReschedule() - if testReshedule fails this test probably will fail too.
-     *
-     * see @testReschedule()
-     */
-    @Test
-    public void testScheduledExecutionTime() throws Exception{
-        // Trigger.timer = new Timer("Jenkins cron thread"); // it should be enought there is no need to start Jenkins
-        GregorianCalendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.MINUTE, 10);
-        int minute = calendar.get(Calendar.MINUTE);
-        //  attribut currentTask should have value calculation
-        TestDiskUsageCalculation calculation = (TestDiskUsageCalculation) new TestDiskUsageCalculation(minute + " * * * *", false);
-        if(calculation.getLastTask()!=null){
-            //should not be any, but if cancel;
-            calculation.getLastTask().cancel();
-        }
-        Long expectedNextExecution = calendar.getTimeInMillis();
-        assertEquals("Scheduled time of disk usage calculation should 0, because calculation is not scheduled", 0, calculation.scheduledLastInstanceExecutionTime(), 60000);
-        Timer.get().schedule(calculation.getNewInstance(), calculation.getRecurrencePeriod(), TimeUnit.MILLISECONDS);
-        assertEquals("Scheduled time of disk usage calculation should be in 10 minutes", expectedNextExecution, calculation.scheduledLastInstanceExecutionTime(), 60000);
-
-        //scheduled time should be changed if configuration of cron is changed
-        calendar.add(Calendar.MINUTE, 10);
-        minute = calendar.get(Calendar.MINUTE);
-        calculation.setCron(minute + " * * * *");
-        calculation.reschedule();
-        expectedNextExecution = calendar.getTimeInMillis();
-        assertEquals("Scheduled time of disk usage calculation should be changed", expectedNextExecution, calculation.scheduledLastInstanceExecutionTime(), 60000);
-
-    }
-
 }
